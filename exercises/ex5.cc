7967c3b82b52a48c94c3ec0fb9cb2e892af38692
Adam Klein <aklein47@cs.washington.edu>
2023-04-17 14:53:19 -0700

    ex5 done

---
+++ exercises/ex5.cc
@@ -0,0 +1,27 @@
+// Copyright 2023 Adam Klein
+// Name: Adam Klein
+// UWemail: aklein47@uw.edu
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+    if (argc != 1) {  // checks for if exactly 1 paramiter was passed in
+        std::cerr << "ERROR: incorect number of inputs\n";
+        return EXIT_FAILURE;
+    }
+    int num;
+    std::cout << "Which positive integer would you like me to factorize? ";
+    std::cin >> num;
+    if (num <= 0) {  // sees if num is an int
+        std::cerr << "ERROR: input not a natual number (math)\n";
+        return EXIT_FAILURE;
+    }
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
+            std::cout << i << " ";
+        }
+    }
+    std::cout << num << "\n";
+    return EXIT_SUCCESS;
+}
