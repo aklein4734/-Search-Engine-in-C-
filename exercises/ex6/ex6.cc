539c9d5d088dec375fc1c583d7ea639406080513
Adam Klein <aklein47@cs.washington.edu>
2023-04-22 00:29:10 -0700

    worked on tests for ex6

--- exercises/ex6/ex6.cc
+++ exercises/ex6/ex6.cc
@@ -8,5 +8,26 @@
 #include "Vector.h"
 
 int main(int argc, char* argv[]) {
+  Vector v = Vector(1.0, 2.0, 3.0);
+  if (!Test_Gets(v, 1.0, 2.0, 3.0)) {
 
+  }
+  if (!Test_Constuct(v)) {
+
+  }
+}
+
+static bool Test_Constuct(Vector v) {
+  if (&v == NULL) {
+    return false;
+  }
+  Vector orgin = Vector();
+  return true;
+}
+
+static bool Test_Gets(Vector v, float x, float y, float z) {
+  if (v.get_x() != x || v.get_y() != y || v.get_z() != z) {
+    return false;
+  }
+  return true;
 }
