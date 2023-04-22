f811f0dfd1110feec7e357146108649b15069eb5
Adam Klein <aklein47@cs.washington.edu>
2023-04-22 15:55:24 -0700

    worked on ex6.cc

--- exercises/ex6/ex6.cc
+++ exercises/ex6/ex6.cc
@@ -6,28 +6,41 @@
 #include <cstdlib>
 
 #include "Vector.h"
+#define x1 1.0
+#define y1 2.0
+#define z1 3.0
 
 int main(int argc, char* argv[]) {
-  Vector v = Vector(1.0, 2.0, 3.0);
-  if (!Test_Gets(v, 1.0, 2.0, 3.0)) {
-
-  }
-  if (!Test_Constuct(v)) {
-
+  Vector v = Vector(x1, y1, z1);
+  if (!Test_Gets(v) || !Test_Constuct(v)) {
+    return EXIT_FAILURE;
   }
 }
 
 static bool Test_Constuct(Vector v) {
   if (&v == NULL) {
+    std::cerr << "ERROR: Your get functions are wrorg!";
     return false;
   }
-  Vector orgin = Vector();
   return true;
 }
 
-static bool Test_Gets(Vector v, float x, float y, float z) {
-  if (v.get_x() != x || v.get_y() != y || v.get_z() != z) {
+static bool Test_Gets(Vector v) {
+  if (v.get_x() != x1 || v.get_y() != y1 || v.get_z() != z1) {
+    std::cerr << "ERROR: get functions failed";
     return false;
   }
+  return true;
+}
+
+static bool Test_Set() {
+  return true;
+}
+
+static bool Test_add() {
+  return true;
+}
+
+static bool Test_mult() {
   return true;
 }
