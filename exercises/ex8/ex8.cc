67bd64e4454725c0b029f2068bfbc0522306257f
Adam Klein <aklein47@cs.washington.edu>
2023-05-08 22:47:32 -0700

    ex8 done

--- exercises/ex8/ex8.cc
+++ exercises/ex8/ex8.cc
@@ -8,30 +8,43 @@
 #include <string>
 #include <map>
 
+// finds wether the in is still good to read and returns the next value
+template <typename T> bool ReadValue(std::ifstream& in, T* const output);
+
 int main(int argc, char* argv[]) {
-     // TODO: Write this function
   if (argc != 2) {  // checks for if exactly 1 paramiter was passed in
     std::cerr << "ERROR: incorect number of inputs" << std::endl;
     return EXIT_FAILURE;
   }
-  errno = 0;
   std::ifstream file;
   file.open(argv[1], std::ifstream::in);
-  if (file.bad()) {
+  if (file.bad()) {  // sees if the ifsteam is good
     std::cerr << "Incorect file path passed in" << std::endl;
     file.close();
     return EXIT_FAILURE;
   }
   std::map<std::string, int> map;
-
-  char buf[100];
-  while (!feof(file)) {
-    if (fgets(buf , 100 , file) == NULL) {
-      break;
-    }
-    fputs(buf, stdout);
+  std::map<std::string, int>::iterator it;
+  std::string *word = new std::string();
+  while (ReadValue(file, word)) {
+    map[*word]++;
+  }
+  for (it = map.begin(); it != map.end(); ++it) {
+    std::cout << it->first << " " << it->second << std::endl;
   }
   file.close();
-  
+  delete word;
   return EXIT_SUCCESS;
+}
+
+template <typename T> bool ReadValue(std::ifstream& in, T* const output) {
+  if (in.fail()) {  // cheacks if ifstream in fails
+    std::cerr << "File read failed" << std::endl;
+    in.close();
+    exit(EXIT_FAILURE);
+  }
+  T prev = *output;
+  in >> *output;
+  // checks to see if pointer is at the same spot
+  return in.good() || prev != *output;
 }
